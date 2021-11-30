10c4436c7a5db2170737aa6fed15a8e22242b633
Ken Courville <krcourville@users.noreply.github.com>
2021-11-30 15:11:56 -0700

    cleanup

--- src/routes/runbooks.svelte
+++ src/routes/runbooks.svelte
@@ -1,7 +1,6 @@
 <script>
 	import Runbooks from '../components/runbooks.md';
 	import Slideshow from '../components/slideshow.svelte';
-	// import slideshow from '../actions/slideshow';
 </script>
 
 <Slideshow presentation={Runbooks} />
